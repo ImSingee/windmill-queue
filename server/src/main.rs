86c3d5c664bc85376d84bf598fc8e8c958dd302f
Bryan <git@singee.me>
2024-02-03 16:05:44 +0800

    server: init with tokio + axum

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use clap::{arg, command};
+
+use clap::Parser;
+use tokio::net::TcpListener;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, env = "WQ_LISTEN", help = "The address to listen on", default_value = "0.0.0.0:8050")]
+    listen: String,
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let args = Args::parse();
+
+    let app = Router::new()
+        .route("/", get(root));
+
+
+    println!("Listen on: {}", args.listen);
+
+    let listener = TcpListener::bind(args.listen).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
 }
