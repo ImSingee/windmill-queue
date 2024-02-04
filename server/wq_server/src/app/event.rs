4e9d4e6d5521ff2d03129666822b9993046d21b8
Bryan <git@singee.me>
2024-02-04 13:42:24 +0800

    server: ingest_events convert

--- server/wq_server/src/app/event.rs
+++ server/wq_server/src/app/event.rs
@@ -2,12 +2,14 @@ use serde_json::{Map as JsonMap, Value as JsonValue};
 
 pub type AnyMessage = JsonMap<String, JsonValue>;
 
+#[derive(Debug)]
 pub struct Event {
     pub queue: String,
     pub message: AnyMessage,
     pub meta: EventMeta,
 }
 
+#[derive(Debug)]
 pub struct EventMeta {
     pub id: String,
     pub ts: u64,
--- server/wq_server/src/routes/events.rs
+++ server/wq_server/src/routes/events.rs
@@ -1,10 +1,13 @@
+use pavex::http::StatusCode;
 use pavex::request::body::JsonBody;
 use pavex::request::path::PathParams;
 use pavex::response::Response;
 use serde::Deserialize;
 use serde_json::json;
-use crate::app::event::AnyMessage;
-use crate::utils::pavex::json_response;
+use thiserror::Error;
+use uuid::Uuid;
+use crate::app::event::{AnyMessage, Event, EventMeta};
+use crate::utils::pavex::{json_response, json_response_with_status};
 
 #[PathParams]
 pub struct Params {
@@ -12,7 +15,7 @@ pub struct Params {
 }
 
 #[derive(Deserialize, Debug)]
-pub struct InEvents {
+pub struct InEvent {
     pub message: AnyMessage,
     #[serde(default = "Default::default")]
     pub meta: InMeta,
@@ -26,11 +29,50 @@ pub struct InMeta {
     pub ts: Option<u64>,
 }
 
+#[derive(Debug, Error)]
+enum EventConvertError {
+    #[error("idKey {0} does not exist in provided event")]
+    IdKeyNotExist(String)
+}
 
-pub async fn ingest_events(PathParams(Params { queue }): &PathParams<Params>, body: JsonBody<Vec<InEvents>>) -> Response {
-    println!("Ingesting events into queue: {}", queue);
-    for event in body.0.iter() {
-        println!("Event: {:?}", event);
+impl InEvent {
+    fn into_event(self, queue: &str) -> Result<Event, EventConvertError> {
+        let ts = self.meta.ts.unwrap_or_else(|| chrono::Utc::now().timestamp_millis() as u64);
+
+        let id = if let Some(id) = self.meta.id {
+            id
+        } else if let Some(id_key) = self.meta.id_key {
+            if let Some(id) = self.message.get(&id_key) {
+                id.as_str().ok_or_else(|| EventConvertError::IdKeyNotExist(id_key.clone()))?.to_string()
+            } else {
+                return Err(EventConvertError::IdKeyNotExist(id_key));
+            }
+        } else {
+            Uuid::now_v7().to_string()
+        };
+
+        Ok(Event {
+            queue: queue.to_string(),
+            message: self.message,
+            meta: EventMeta {
+                id,
+                ts,
+            },
+        })
+    }
+}
+
+pub async fn ingest_events(PathParams(Params { queue }): &PathParams<Params>, JsonBody(events): JsonBody<Vec<InEvent>>) -> Response {
+    let events = events.into_iter().map(|event| event.into_event(&queue)).collect::<Result<Vec<_>, _>>();
+    let events = match events {
+        Ok(events) => events,
+        Err(err) => {
+            return json_response_with_status(StatusCode::BAD_REQUEST, json!({"success": false, "error": err.to_string()}));
+        }
+    };
+
+    for event in events {
+        println!("Ingest Event: {:?}", event);
     }
 
     json_response(json!({"success": true}))
