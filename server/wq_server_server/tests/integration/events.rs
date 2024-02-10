c9dff614154e95e8a5bd90e0ece595299bdd3e63
Bryan <git@singee.me>
2024-02-11 00:25:23 +0800

    server: check if events inserted

--- server/wq_server_server/tests/integration/events.rs
+++ server/wq_server_server/tests/integration/events.rs
@@ -1,74 +1,97 @@
+use crate::helpers::{ResponseExt, TestApi};
 use pavex::http::StatusCode;
 use serde_json::json;
-use crate::helpers::{ResponseExt, TestApi};
+use wq_server::app::db::sea_orm::DatabaseBackend::Postgres;
+use wq_server::app::db::sea_orm::{ConnectionTrait, Statement};
 
 #[tokio::test]
 async fn insert_events_works() {
     let api = TestApi::spawn().await;
 
-    let response = api.ingest_events(json!({
-        "events": [
-            {
-                "event": {
-                    "hello": 1
-                }
-            },
-            {
-                "event": {
-                    "hello": 2
+    let response = api
+        .ingest_events(json!({
+            "events": [
+                {
+                    "event": {
+                        "hello": 1
+                    }
                 },
-                "meta": {
-                    "id": "123",
-                }
-            },
-            {
-                "event": {
-                    "id": "666",
-                    "hello": 1
+                {
+                    "event": {
+                        "hello": 2
+                    },
+                    "meta": {
+                        "id": "123",
+                    }
                 },
-                "meta": {
-                    "idKey": "id",
-                }
-            },
-            {
-                "event": {
-                    "hello": 1
+                {
+                    "event": {
+                        "id": "666",
+                        "hello": 1
+                    },
+                    "meta": {
+                        "idKey": "id",
+                    }
                 },
-                "meta": {
-                    "ts": 1707177603744u64
-                }
-            },
-        ],
-        "meta": {
-            "queue": "test",
-        }
-    })).await;
+                {
+                    "event": {
+                        "hello": 1
+                    },
+                    "meta": {
+                        "ts": 1707177603744u64
+                    }
+                },
+            ],
+            "meta": {
+                "queue": "test",
+            }
+        }))
+        .await;
 
     let status = response.status();
 
     assert_eq!(status.as_u16(), StatusCode::OK.as_u16());
+
+    // wait 100ms for executor to process the events
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+
+    // check if events are inserted
+    let count = api
+        .db
+        .query_one(Statement::from_string(
+            Postgres,
+            "SELECT COUNT(*) FROM events",
+        ))
+        .await
+        .unwrap()
+        .unwrap()
+        .try_get_by_index::<i64>(0)
+        .unwrap();
+    assert_eq!(count, 4);
 }
 
 #[tokio::test]
 async fn insert_events_fail() {
     let api = TestApi::spawn().await;
 
-    let response = api.ingest_events(json!({
-        "events": [
-            {
-                "event": {
-                    "id": "666",
-                    "hello": 1
-                },
-                "meta": {
-                    "idKey": "not-exist",
+    let response = api
+        .ingest_events(json!({
+            "events": [
+                {
+                    "event": {
+                        "id": "666",
+                        "hello": 1
+                    },
+                    "meta": {
+                        "idKey": "not-exist",
+                    }
                 }
+            ],
+            "meta": {
+                "queue": "test",
             }
-        ],
-        "meta": {
-            "queue": "test",
-        }
-    })).await;
+        }))
+        .await;
 
     let status = response.status();
 
